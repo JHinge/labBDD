939c47c2ef67806ffaf95082e5de8a683943fbb0
Zawiasa.Jakub <zawiasa.jakub@gmail.com>
2019-05-31 16:49:23 +0200

    feature1_impl BasicItinetaryService

---
+++ src/main/java/edu/iis/mto/bdd/trains/services/BasicItineraryService.java
@@ -0,0 +1,20 @@
+package edu.iis.mto.bdd.trains.services;
+
+import java.util.List;
+
+import org.joda.time.LocalTime;
+
+
+public class BasicItineraryService implements IntineraryService {
+
+    public BasicItineraryService() {
+
+    }
+
+    @Override
+    public List<LocalTime> findNextDepartures(String departure, String destination, LocalTime startTime) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
